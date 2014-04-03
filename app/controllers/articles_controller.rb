f24c5075b1280057e6728802c7df334139cfc8b7
NoRest4AWhearry <awhearry@gmail.com>
2014-04-03 16:13:09 -0400

    Competed all article controller methods and corresponding views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,44 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash.notice = "Article '#{@article.title}' created!"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash.notice = "Article '#{@article.title}' updated!"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id]).destroy
+		flash.notice = "Article '#{@article.title}' was deleted!"
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
 end
